65fcdd402933d80ff25520ff97f60bf49f2228cf
eric496 <eric.mlengineer@gmail.com>
2018-02-08 22:33:44 -0500

    add solution

--- arrays/217_contains_duplicate.java
+++ arrays/217_contains_duplicate.java
@@ -2,6 +2,7 @@
 Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 */
 
+// method 1
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         if (nums == null || nums.length == 0) return false;
@@ -11,6 +12,22 @@ class Solution {
                 return true;
             } else {
                 map.put(i, 1);
+            }
+        }
+        return false;
+    }
+}
+
+// method 2
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            } else {
+                set.add(n);
             }
         }
         return false;
