8c61fc77411e4c6f36c104f007dd2719cc47d381
eric496 <eric.mlengineer@gmail.com>
2018-02-12 23:41:06 -0500

    add solution

--- strings/242_valid_anagrams.java
+++ strings/242_valid_anagrams.java
@@ -11,3 +11,20 @@ You may assume the string contains only lowercase alphabets.
 Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 */
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] result = new int[26];
+        for (char c : s.toCharArray()) {
+            result[c-'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            result[c-'a']--;
+        }
+        for (int i : result) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+}
