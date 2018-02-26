e02e226e25585cdd817ef9d76751390e5def8c15
eric496 <eric.mlengineer@gmail.com>
2018-02-25 23:45:59 -0500

    add 231

---
+++ bit_manipulation/231_power_of_two.java
@@ -0,0 +1,10 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+*/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return n > 0 && ((n&(n-1)) == 0);
+    }
+}
